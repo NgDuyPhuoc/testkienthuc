f2d9d545cdb4d9eef5fd1a356abf720e7499615d
NgDuyPhuoc <ngduyphuoc25082006@gmail.com>
2024-07-15 19:43:12 +0700

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 7;
+let c = -2;
+
+console.log(`Day la dap an: ${a+b+c}, ${a-b-c}`)
+
+let text = "database su xong";
+
+console.log(`Số kí tự ==> ${text.split( ).length}`);
+console.log(`Số dư ==> ${text.replace().length%2}`);
+
